fe0744a8644f050594fc9243be2f6afa52d90264
Bojan <bojan-stanimirovski097@hotmail.com>
2022-12-25 13:32:27 +0100

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -51,7 +51,7 @@ export default function Home() {
               </li>
               <li>
                 <a
-                  className=" bg-gradient-to-r from-cyan-600 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
+                  className="bg-gradient-to-r from-cyan-600 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
                   href="#"
                 >
                   Resume
